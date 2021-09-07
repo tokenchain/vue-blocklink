5d49522b541404229ab60461b23fc9d61105ec52
heskdx <jobhesk@gmail.com>
2021-09-08 02:20:27 +0800

    Please check!  4 files changed, 7 insertions(+), 16 deletions(-)

--- src/index.js
+++ src/index.js
@@ -2,27 +2,18 @@ import BlockWrap from "./abi/BlockWrap";
 import EthereumWeb3Component from "./mixins/vue-metamask"
 import ImTokenComponent from "./mixins/vue-imtoken"
 import VueTypesWeb3 from "./base/eth/types"
+import {BigNumber} from "./base/eth/utils/configured_bignumber"
 import {
     fromTokenUnitAmount,
     toTokenUnitAmount
 } from "./base/eth/utils"
-
-/**
- import ETHTypes from "./base/eth/types"
- import EthBaseContract from "./base/eth/base"
- import EthAssert from "./base/eth/0xassert"
- import ETHValidSchema from "./base/eth/validations"
- import ETHWeb3 from "./base/eth/0xw3w"
- import ETH0xType from "./base/eth/0xtypes"
- import ETHUtil from "./base/eth/utils"
- import ETHSubscriptionManager from "./base/eth/subscription_manager"
- **/
 
 export {
     BlockWrap,
     VueTypesWeb3,
     ImTokenComponent,
     EthereumWeb3Component,
+    BigNumber,
     fromTokenUnitAmount,
     toTokenUnitAmount
 }
