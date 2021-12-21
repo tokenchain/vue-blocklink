3a7c899581e86e17bb6415c8bb41318474049faa
heskdx <jobhesk@gmail.com>
2021-12-21 12:53:45 +0800

    Please check!  4 files changed, 43 insertions(+), 15 deletions(-)

--- src/utils/safemath.js
+++ src/utils/safemath.js
@@ -233,6 +233,29 @@ function Base64() {
 }
 
 
+function toFixedBn(x) {
+    if (Math.abs(x) < 1.0) {
+        let e = parseInt(x.toString().split('e-')[1]);
+        if (e) {
+            x *= Math.pow(10,e-1);
+            x = '0.' + (new Array(e)).join('0') + x.toString().substring(2);
+        }
+    } else {
+        let e = parseInt(x.toString().split('+')[1]);
+        if (e > 20) {
+            e -= 20;
+            x /= Math.pow(10,e);
+            x += (new Array(e+1)).join('0');
+        }
+    }
+    return x;
+}
+
+
+function toWad(x){
+    return toFixedBn(x * 1e18)
+}
+
 
 export default {
     shuffle,
@@ -249,6 +272,8 @@ export default {
     HALF_PI,
     TAU,
     TO_RAD,
+    toFixedBn,
+    toWad,
     floor,
     rand,
     randIn,
